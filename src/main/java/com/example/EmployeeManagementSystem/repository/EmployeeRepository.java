484bf60ffd8707462f29714fdf03daa9ed343b89
MalshaSamarakoon <chamodi96samarakoon@gmail.com>
2023-04-18 11:57:01 +0530

    Add repository

--- src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-      Optional<Employee> findById(String email);
+public interface EmployeeRepository extends JpaRepository<Employee, Long> { // <entity, primarykey type>
+      Optional<Employee> findById(long id);
 
 }
